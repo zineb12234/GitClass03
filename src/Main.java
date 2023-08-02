61c0ec123cf623e5658648da71e4a4224debd746
zineb12234 <zinebkasimi1@gmail.com>
2023-08-02 19:59:51 -0400

    the cod that tells me avialabillty of the product

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+ public class Main{
+    public static void main(String[] args) {
+        boolean productAvailable = true;
+
+        if (productAvailable) {
+            System.out.print("yes");
+        } else {
+            System.out.print("no");
+        }
+    }
